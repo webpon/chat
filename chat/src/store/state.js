a1f9d7bf954edd2fdc8952f6308173e5f0282e2c
lwj <210579781@qq.com>
2022-10-11 13:06:40 +0800

    添加默认时间字段

--- chat/src/store/state.js
+++ chat/src/store/state.js
@@ -9,13 +9,15 @@ export default {
             username: '智能客服',
             msg: '欢迎反馈bug',
             imgSrc: 'https://webpon-img.oss-cn-guangzhou.aliyuncs.com/avater/avater/1.jpg',
-            msgNumber: 0
+            msgNumber: 0,
+            time: new Date().getTime()
         },
         {
             username: '群聊',
             msg: '在这里可以收到所有人的信息',
             imgSrc: 'https://webpon-img.oss-cn-guangzhou.aliyuncs.com/avater/avater/4.jpg',
-            msgNumber: 0
+            msgNumber: 0,
+            time: new Date().getTime()
         }
     ],
     playingVideo: null,
