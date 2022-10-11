6c80efb0d14f610df0fa426f2f7f67b579c2d572
lwj <210579781@qq.com>
2022-10-11 11:19:17 +0800

    修改默认信息列表

--- chat/src/store/state.js
+++ chat/src/store/state.js
@@ -4,7 +4,20 @@ export default {
     msgInfo: JSON.parse(localStorage.msgInfo || 'null') || null,
     myInfo: JSON.parse(localStorage.getItem('myInfo')),
     contacts: [],
-    chatList: JSON.parse(localStorage.getItem('chatList'))||[],
+    chatList: JSON.parse(localStorage.getItem('chatList'))||[
+        {
+            username: '智能客服',
+            msg: '欢迎反馈bug',
+            imgSrc: 'https://webpon-img.oss-cn-guangzhou.aliyuncs.com/avater/avater/1.jpg',
+            msgNumber: 0
+        },
+        {
+            username: '群聊',
+            msg: '在这里可以收到所有人的信息',
+            imgSrc: 'https://webpon-img.oss-cn-guangzhou.aliyuncs.com/avater/avater/4.jpg',
+            msgNumber: 0
+        }
+    ],
     playingVideo: null,
     isMobile: false,
     msgHint: {},
